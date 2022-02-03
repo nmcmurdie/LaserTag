e9474b0ddbcf3bad5e19134546e8e31bb88aff4d
Nick McMurdie <nickmcmurdie@gmail.com>
2022-02-03 14:48:21 -0700

    Add power functions

--- lasertag/filter.c
+++ lasertag/filter.c
@@ -3,32 +3,30 @@
 #define FILTER_IIR_COUNT 10
 #define IIR_A_COEFFICIENT_COUNT 10
 #define Z_QUEUE_SIZE IIR_A_COEFFICIENT_COUNT
-#define OUTPUT_QUEUE_SIZE IIR_A_COEFFICIENT_COUNT
+#define OUTPUT_QUEUE_SIZE 2000
 #define X_QUEUE_SIZE 81
-#define Y_QUEUE_SIZE 81
+#define Y_QUEUE_SIZE IIR_B_COEFFICIENT_COUNT
 #define QUEUE_INIT_VAL 0
+#define FIR_COEFFICIENT_COUNT 81
+#define FILTER_FREQUENCY_COUNT 10
+#define IIR_A_COEFFICIENT_COUNT 10
+#define IIR_B_COEFFICIENT_COUNT 11
+#define QUEUE_OLDEST_ELEM 0
 
 static queue_t zQueue[FILTER_IIR_COUNT];
 static queue_t outputQueue[FILTER_IIR_COUNT];
 static queue_t xQueue;
 static queue_t yQueue;
-const static double FIRCoefficients[X_QUEUE_SIZE] = {
+static double prevPower, oldestValue;
+static double currentPowerValue[FILTER_FREQUENCY_COUNT];
+const static double FIRCoefficients[FIR_COEFFICIENT_COUNT] = {
     4.3579622275120866e-04, 2.7155425450406482e-04, 6.3039002645022389e-05,
     -1.9349227837935689e-04, -4.9526428865281219e-04, -8.2651441681321381e-04,
     -1.1538970332472540e-03, -1.4254746936265955e-03, -1.5744703111426981e-03,
     -1.5281041447445794e-03, -1.2208092333090719e-03, -6.1008312441271589e-04, 
     3.0761698758506020e-04, 1.4840192333212628e-03, 2.8123077568332064e-03,
-    4.1290616416556000e-03, 5.2263464670258821e-03, 5.8739882867061598e-03, 
-    5.8504032099208096e-03, 4.9787419333799775e-03, 3.1637974805960069e-03,
-    4.2435139609132765e-04, -3.0844289197247210e-03, -7.0632027332701800e-03,
-    -1.1078458037608587e-02, -1.4591395057493114e-02, -1.7004337345765962e-02,
-    -1.7720830774014484e-02, -1.6213409845727566e-02, -1.2091458677988302e-02,
-    -5.1609257765542595e-03, 4.5319860006883522e-03, 1.6679627700682677e-02,
-    3.0718365411587255e-02, 4.5861875593064996e-02, 6.1160185621895728e-02,
-    7.5579213982547147e-02, 8.8092930943210607e-02, 9.7778502396672365e-02,
-    1.0390414346016495e-01
+    4.1290616416556000e-03, 5.2#define OUTPUT_QUEUE_SIZE 2000
 };
-
 const static double iirACoefficientConstants[FILTER_FREQUENCY_COUNT][IIR_A_COEFFICIENT_COUNT] = {
 {-5.9637727070164015e+00, 1.9125339333078248e+01, -4.0341474540744173e+01, 6.1537466875368821e+01, -7.0019717951472188e+01, 6.0298814235238872e+01, -3.8733792862566290e+01, 1.7993533279581058e+01, -5.4979061224867651e+00, 9.0332828533799547e-01},
 {-4.6377947119071505e+00, 1.3502215749461598e+01, -2.6155952405269829e+01, 3.8589668330738476e+01, -4.3038990303252795e+01, 3.7812927599537275e+01, -2.5113598088113893e+01, 1.2703182701888142e+01, -4.2755083391143689e+00, 9.0332828533800569e-01},
@@ -41,7 +39,6 @@ const static double iirACoefficientConstants[FILTER_FREQUENCY_COUNT][IIR_A_COEFF
 {7.4092912870072398e+00, 2.6857944460290135e+01, 6.1578787811202247e+01, 9.8258255839887312e+01, 1.1359460153696298e+02, 9.6280452143026082e+01, 5.9124742025776392e+01, 2.5268527576524203e+01, 6.8305064480743081e+00, 9.0332828533799969e-01},
 {8.5743055776347692e+00, 3.4306584753117889e+01, 8.4035290411037053e+01, 1.3928510844056814e+02, 1.6305115418161620e+02, 1.3648147221895786e+02, 8.0686288623299745e+01, 3.2276361903872115e+01, 7.9045143816244696e+00, 9.0332828533799636e-01}
 };
-
 const static double iirBCoefficientConstants[FILTER_FREQUENCY_COUNT][IIR_B_COEFFICIENT_COUNT] = {
 {9.0928661148194738e-10, 0.0000000000000000e+00, -4.5464330574097372e-09, 0.0000000000000000e+00, 9.0928661148194745e-09, 0.0000000000000000e+00, -9.0928661148194745e-09, 0.0000000000000000e+00, 4.5464330574097372e-09, 0.0000000000000000e+00, -9.0928661148194738e-10},
 {9.0928661148175165e-10, 0.0000000000000000e+00, -4.5464330574087587e-09, 0.0000000000000000e+00, 9.0928661148175174e-09, 0.0000000000000000e+00, -9.0928661148175174e-09, 0.0000000000000000e+00, 4.5464330574087587e-09, 0.0000000000000000e+00, -9.0928661148175165e-10},
@@ -119,7 +116,12 @@ void filter_addNewInput(double x) {
 // after executing this function, the queue will contain 10 values
 // all of them 1.0.
 void filter_fillQueue(queue_t *q, double fillValue) {
-    
+    queue_size_t size = queue_size(q);
+
+    for (uint32_t i = 0; i < size; i++) {
+        // Overwrite contents of queue with the fill value
+        queue_overwritePush(fillValue);
+    }
 }
 
 // Invokes the FIR-filter. Input is contents of xQueue.
@@ -128,6 +130,7 @@ double filter_firFilter() {
     double output = 0;
 
     for (uin32_t i = 0; i < X_QUEUE_SIZE; i++) {
+        // Process the queue using the FIR filter coefficients
         output += FIRCoefficients[i] * queue_readElementAt(&xQueue, X_QUEUE_SIZE - 1 - i);
     }
 
@@ -137,7 +140,21 @@ double filter_firFilter() {
 // Use this to invoke a single iir filter. Input comes from yQueue.
 // Output is returned and is also pushed onto zQueue[filterNumber].
 double filter_iirFilter(uint16_t filterNumber) {
+    double output = 0.0;
 
+    for (uin32_t i = 0; i < IIR_B_COEFFICIENT_COUNT; i++) {
+        // Process the queue using the IIR B filter coefficients
+        output += iirBCoefficientConstants[filterNumber][i] * queue_readElementAt(&yQueue, IIR_B_COEFFICIENT_COUNT - 1 - i);
+    }
+
+    for (uin32_t i = 0; i < IIR_OUTPUT_QUEUE_SIZE - 1 - A_COEFFICIENT_COUNT; i++) {
+        // Process the queue using the IIR A filter coefficients
+        output -= iirACoefficientConstants[filterNumber][i] * queue_readElementAt(&yQueue, IIR_A_COEFFICIENT_COUNT - 1 - i);
+    }
+
+    queue_overwritePush(&(zQueue[filterNumber]), output);
+
+    return output;
 }
 
 // Use this to compute the power for values contained in an outputQueue.
@@ -155,13 +172,32 @@ double filter_iirFilter(uint16_t filterNumber) {
 // array to keep track of these values for each of the 10 output queues.
 double filter_computePower(uint16_t filterNumber, bool forceComputeFromScratch,
                            bool debugPrint) {
+    double power = 0.0;
 
+    if (forceComputeFromScratch) {
+        // Compute the power from output queues from scratch
+        for (uint32_t i = 0; i < OUTPUT_QUEUE_SIZE; i++) {
+            // Sum each element in the output queue squared
+            double queueElem = queue_readElementAt(&(outputQueue[filterNumber]), i);
+            power += queueElem * queueElem;
+        }
+    }
+    else {
+        // Not computing from scratch, remove oldest value and add newest value (squared)
+        double newestValue = queue_readElementAt(&(outputQueue[filterNumber]), OUTPUT_QUEUE_SIZE - 1);
+        power = prevPower - (oldestValue * oldestValue) + (newestValue * newestValue);
+    }
+
+    prevPower = power;
+    oldestValue = queue_readElementAt(&(outputQueue[filterNumber]), QUEUE_OLDEST_ELEM);
+    currentPowerValue[filterNumber] = power;
+    return power;
 }
 
 // Returns the last-computed output power value for the IIR filter
 // [filterNumber].
 double filter_getCurrentPowerValue(uint16_t filterNumber) {
-
+    return currentPowerValue[filterNumber];
 }
 
 // Get a copy of the current power values.
@@ -170,7 +206,10 @@ double filter_getCurrentPowerValue(uint16_t filterNumber) {
 // detector. Remember that when you pass an array into a C function, changes to
 // the array within that function are reflected in the returned array.
 void filter_getCurrentPowerValues(double powerValues[]) {
-
+    for (uint16_t i = 0; i < FILTER_FREQUENCY_COUNT; i++) {
+        // Copy values from our power value array into the provided array
+        powerValues[i] = currentPowerValue[i];
+    }
 }
 
 // Using the previously-computed power values that are current stored in
@@ -179,7 +218,20 @@ void filter_getCurrentPowerValues(double powerValues[]) {
 // normalizedArray by the maximum power value contained in currentPowerValue[].
 void filter_getNormalizedPowerValues(double normalizedArray[],
                                      uint16_t *indexOfMaxValue) {
+    double maxValue = 0.0;
+    for (uint16_t i = 0; i < FILTER_FREQUENCY_COUNT; i++) {
+        // Iterate over each power value
+        if (currentPowerValue[i] > maxValue) {
+            // If the power is at its max, keep the value and index
+            *indexOfMaxValue = i;
+            maxValue = currentPowerValue[i];
+        }
+    }
 
+    for (uint16_t i = 0; i < FILTER_FREQUENCY_COUNT; i++) {
+        // Copy over power values and normalize
+        normalizedArray[i] = currentPowerValue[i] / maxValue;
+    }
 }
 
 /*********************************************************************************************************
@@ -193,7 +245,7 @@ void filter_getNormalizedPowerValues(double normalizedArray[],
 
 // Returns the array of FIR coefficients.
 const double *filter_getFirCoefficientArray() {
-
+    
 }
 
 // Returns the number of FIR coefficients.
